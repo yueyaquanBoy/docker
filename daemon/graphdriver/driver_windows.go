a277c95c4ef32447d7da9581032d9eaaeea1b526
John Howard (VM) <John.Howard@microsoft.com>
2015-02-17 10:26:31 -0800

    A bit more debugging in shell Windows graphdriver

--- daemon/graphdriver/driver_windows.go
+++ daemon/graphdriver/driver_windows.go
@@ -2,19 +2,29 @@
 
 package graphdriver
 
+import (
+	log "github.com/Sirupsen/logrus"
+)
+
+const (
+	FsMagicWindows = FsMagic(0xa1b1830f) // I have just made this up for now. NTFS=0x5346544E
+)
+
 var (
 	// Slice of drivers that should be used in an order
 	priority = []string{
-		"windows", // TODO Windows when implemented
+		"windows",
 	}
 
 	FsNames = map[FsMagic]string{
+
+		FsMagicWindows:     "windows",
 		FsMagicUnsupported: "unsupported",
 	}
 )
 
 func GetFSMagic(rootpath string) (FsMagic, error) {
-
+	log.Debugln("WindowsGraphDriver GetFSMagic()")
 	// TODO Windows
 	return 0, nil
 }
