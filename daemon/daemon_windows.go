067f58a172e6c3d331b47b1f756f07c48f75def0
John Howard (VM) <John.Howard@microsoft.com>
2015-02-17 10:25:25 -0800

    Including Windows graphdriver for Windows builds

--- daemon/daemon_windows.go
+++ daemon/daemon_windows.go
@@ -2,6 +2,10 @@
 
 package daemon
 
+import (
+	_ "github.com/docker/docker/daemon/graphdriver/windows"
+)
+
 func KillIfLxc(ID string) {
 	// No-op on Windows as Lxc execution driver is not used.
 }
