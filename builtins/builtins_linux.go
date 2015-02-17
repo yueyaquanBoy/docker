9e98aeae67779fdfe01cf7542c471877f923c4c6
John Howard (VM) <John.Howard@microsoft.com>
2015-02-17 12:42:16 -0800

    Fixed imports for Linux build

--- builtins/builtins_linux.go
+++ builtins/builtins_linux.go
@@ -3,15 +3,8 @@
 package builtins
 
 import (
-	"runtime"
-
-	"github.com/docker/docker/api"
-	apiserver "github.com/docker/docker/api/server"
 	"github.com/docker/docker/daemon/networkdriver/bridge"
-	"github.com/docker/docker/dockerversion"
 	"github.com/docker/docker/engine"
-	"github.com/docker/docker/events"
-	"github.com/docker/docker/pkg/parsers/kernel"
 )
 
 func daemon(eng *engine.Engine) error {
