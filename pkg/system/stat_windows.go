e7d0b76f04a53b47028e34aeecf5ea5e0058527c
John Howard <John.Howard@microsoft.com>
2015-03-03 20:28:04 -0800

    Interim step for implementing stat/lstat on windows. Needs changes in archive\changes.go too
    Signed-off-by: John Howard <John.Howard@microsoft.com>

--- pkg/system/stat_windows.go
+++ pkg/system/stat_windows.go
@@ -3,12 +3,43 @@
 package system
 
 import (
-	"errors"
 	"syscall"
 )
 
-func fromStatT(s *syscall.Win32FileAttributeData) (*Stat_t, error) {
-	return nil, errors.New("fromStatT should not be called on windows path")
+type Stat struct {
+	fileAttributes uint32
+	creationTime   syscall.Filetime
+	lastWriteTime  syscall.Filetime
+	fileSizeHigh   uint32
+	fileSizeLow    uint32
+}
+
+func (s Stat) FileAttributes() uint32 {
+	return s.fileAttributes
+}
+
+func (s Stat) CreationTime() syscall.Filetime {
+	return s.creationTime
+}
+
+func (s Stat) LastWriteTime() syscall.Filetime {
+	return s.lastWriteTime
+}
+
+func (s Stat) FileSizeHigh() uint32 {
+	return s.fileSizeHigh
+}
+
+func (s Stat) FileSizeLow() uint32 {
+	return s.fileSizeLow
+}
+
+func fromStatT(s *syscall.Win32FileAttributeData) (*Stat, error) {
+	return &Stat{fileAttributes: s.FileAttributes,
+		creationTime:  s.CreationTime,
+		lastWriteTime: s.LastWriteTime,
+		fileSizeHigh:  s.FileSizeHigh,
+		fileSizeLow:   s.FileSizeLow}, nil
 }
 
 func Stat(path string) (*Stat_t, error) {
