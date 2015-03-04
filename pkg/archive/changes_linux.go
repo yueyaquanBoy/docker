1108b6e1de5aa20015c36675c1a8bde985baa44e
John Howard <John.Howard@microsoft.com>
2015-03-03 21:08:45 -0800

    Refactoring for stat() differences on Windows vs Linux
    Signed-off-by: John Howard <John.Howard@microsoft.com>

---
+++ pkg/archive/changes_linux.go
@@ -0,0 +1,34 @@
+package archive
+
+import (
+	"syscall"
+
+	"github.com/docker/docker/pkg/system"
+)
+
+func (info *FileInfo) isDir() bool {
+	return info.parent == nil || info.stat.Mode()&syscall.S_IFDIR == syscall.S_IFDIR
+}
+
+type Stat struct {
+	mode uint32
+	uid  uint32
+	gid  uint32
+	rdev uint64
+	size int64
+	mtim syscall.Timespec
+}
+
+func statDifferent(oldStat *system.Stat, newStat *system.Stat) bool {
+	// Don't look at size for dirs, its not a good measure of change
+	if oldStat.Mode() != newStat.Mode() ||
+		oldStat.Uid() != newStat.Uid() ||
+		oldStat.Gid() != newStat.Gid() ||
+		oldStat.Rdev() != newStat.Rdev() ||
+		(oldStat.Size() != newStat.Size() && oldStat.Mode()&syscall.S_IFDIR != syscall.S_IFDIR) ||
+		!sameFsTimeSpec(oldStat.Mtim(), newStat.Mtim()) {
+		return true
+	} else {
+		return false
+	}
+}
---
+++ pkg/archive/changes_windows.go
@@ -0,0 +1,25 @@
+package archive
+
+import (
+	"syscall"
+
+	"github.com/docker/docker/pkg/system"
+)
+
+func (info *FileInfo) isDir() bool {
+	return info.parent == nil || info.stat.FileAttributes()&syscall.FILE_ATTRIBUTE_DIRECTORY == syscall.FILE_ATTRIBUTE_DIRECTORY
+}
+
+func statDifferent(oldStat *system.Stat, newStat *system.Stat) bool {
+	// Don't look at size for dirs, its not a good measure of change
+	if oldStat.CreationTime() != newStat.CreationTime() ||
+		oldStat.FileAttributes() != newStat.FileAttributes() ||
+		oldStat.FileSizeHigh() != newStat.FileSizeHigh() && oldStat.FileAttributes()&syscall.FILE_ATTRIBUTE_DIRECTORY != syscall.FILE_ATTRIBUTE_DIRECTORY ||
+		oldStat.FileSizeLow() != newStat.FileSizeLow() && oldStat.FileAttributes()&syscall.FILE_ATTRIBUTE_DIRECTORY != syscall.FILE_ATTRIBUTE_DIRECTORY ||
+		oldStat.LastWriteTime().HighDateTime != oldStat.LastWriteTime().HighDateTime ||
+		oldStat.LastWriteTime().LowDateTime != oldStat.LastWriteTime().LowDateTime {
+		return true
+	} else {
+		return false
+	}
+}
