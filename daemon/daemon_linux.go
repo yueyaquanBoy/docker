27b105ae2ddb1eac9e83740cb6b74ff8461a6a5b
John Howard <John.Howard@microsoft.com>
2015-03-11 14:59:59 -0700

    Windows: Fixing up after bad merge with origin/master
    Signed-off-by: John Howard <John.Howard@microsoft.com>

--- daemon/daemon_linux.go
+++ daemon/daemon_linux.go
@@ -76,7 +76,7 @@ func NewDaemonFromDirectory(config *Config, eng *engine.Engine) (*Daemon, error)
 
 	// Check that the system is supported and we have sufficient privileges
 	if runtime.GOOS != "linux" {
-		return nil, fmt.Errorf("The Docker daemon is only supported on linux")
+		return nil, fmt.Errorf("The Docker daemon is only supported on Linux and Windows")
 	}
 	if os.Geteuid() != 0 {
 		return nil, fmt.Errorf("The Docker daemon needs to be run as root")
