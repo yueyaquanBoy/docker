b5756f6f7333c13e7cda9271f9cddea9f24a17df
John Howard <John.Howard@microsoft.com>
2015-03-11 11:43:43 -0700

    Windows: Fixing stat after bad merge with origin/master
    Signed-off-by: John Howard <John.Howard@microsoft.com>

--- pkg/archive/changes_windows.go
+++ pkg/archive/changes_windows.go
@@ -1,27 +1,21 @@
 package archive
 
 import (
-	"syscall"
-
 	"github.com/docker/docker/pkg/system"
 )
 
-func (info *FileInfo) isDir() bool {
-	return info.parent == nil || info.stat.IsDir()
-	// TODO WINDOWS JJH Think I want the above line. Verify.
-	//return info.parent == nil || info.stat.FileAttributes()&syscall.FILE_ATTRIBUTE_DIRECTORY == syscall.FILE_ATTRIBUTE_DIRECTORY
-}
-
 func statDifferent(oldStat *system.Stat, newStat *system.Stat) bool {
+
 	// Don't look at size for dirs, its not a good measure of change
-	if oldStat.CreationTime() != newStat.CreationTime() ||
-		oldStat.FileAttributes() != newStat.FileAttributes() ||
-		oldStat.FileSizeHigh() != newStat.FileSizeHigh() && oldStat.FileAttributes()&syscall.FILE_ATTRIBUTE_DIRECTORY != syscall.FILE_ATTRIBUTE_DIRECTORY ||
-		oldStat.FileSizeLow() != newStat.FileSizeLow() && oldStat.FileAttributes()&syscall.FILE_ATTRIBUTE_DIRECTORY != syscall.FILE_ATTRIBUTE_DIRECTORY ||
-		oldStat.LastWriteTime().HighDateTime != oldStat.LastWriteTime().HighDateTime ||
-		oldStat.LastWriteTime().LowDateTime != oldStat.LastWriteTime().LowDateTime {
+	if oldStat.ModTime() != newStat.ModTime() ||
+		oldStat.Mode() != newStat.Mode() ||
+		oldStat.Size() != newStat.Size() && !oldStat.IsDir() {
 		return true
 	} else {
 		return false
 	}
+}
+
+func (info *FileInfo) isDir() bool {
+	return info.parent == nil || info.stat.IsDir()
 }
