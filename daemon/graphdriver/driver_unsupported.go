f4d213d03d9092bf63188aa88cfd246731b6ca80
Stefan J. Wernli <swernli@microsoft.com>
2015-03-12 11:33:49 -0700

    Fixing cross compilation
    Signed-off-by: Stefan Wernli <swernli@microsoft.com>

--- daemon/graphdriver/driver_unsupported.go
+++ daemon/graphdriver/driver_unsupported.go
@@ -2,6 +2,13 @@
 
 package graphdriver
 
+var (
+	// Slice of drivers that should be used in an order
+	priority = []string{
+		"unsupported",
+	}
+)
+
 func GetFSMagic(rootpath string) (FsMagic, error) {
 	return FsMagicUnsupported, nil
 }
