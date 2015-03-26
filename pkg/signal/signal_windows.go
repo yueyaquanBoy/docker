f4ceff573f502dc0b129f3b25a7c4a6e5a37481a
John Howard (VM) <John.Howard@microsoft.com>
2015-03-26 14:56:31 -0700

    Windows: Attempt to get signal handling
    Signed-off-by: John Howard (VM) <John.Howard@microsoft.com>

--- pkg/signal/signal_windows.go
+++ pkg/signal/signal_windows.go
@@ -10,3 +10,19 @@ import (
 // invalid signals so they don't get handled)
 const SIGCHLD = syscall.Signal(0xff)
 const SIGWINCH = syscall.Signal(0xff)
+
+var SignalMap = map[string]syscall.Signal{
+	"HUP":  syscall.SIGHUP,
+	"INT":  syscall.SIGINT,
+	"QUIT": syscall.SIGQUIT,
+	"ILL":  syscall.SIGILL,
+	"TRAP": syscall.SIGTRAP,
+	"ABRT": syscall.SIGABRT,
+	"BUS":  syscall.SIGBUS,
+	"FPE":  syscall.SIGFPE,
+	"KILL": syscall.SIGKILL,
+	"SEGV": syscall.SIGSEGV,
+	"PIPE": syscall.SIGPIPE,
+	"ALRM": syscall.SIGALRM,
+	"TERM": syscall.SIGTERM,
+}
