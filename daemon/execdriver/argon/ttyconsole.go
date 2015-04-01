01c8cd82ac02758b1eb066aba50ec304fad158db
John Howard (VM) <John.Howard@microsoft.com>
2015-04-01 16:15:12 -0700

    Windows: Oops, fixed previous commit. And debug logging
    Signed-off-by: John Howard (VM) <John.Howard@microsoft.com>

--- daemon/execdriver/argon/ttyconsole.go
+++ daemon/execdriver/argon/ttyconsole.go
@@ -3,6 +3,7 @@
 package argon
 
 import (
+	log "github.com/Sirupsen/logrus"
 	"github.com/docker/docker/daemon/execdriver"
 )
 
@@ -12,13 +13,13 @@ type TtyConsole struct {
 
 func NewTtyConsole(processConfig *execdriver.ProcessConfig, pipes *execdriver.Pipes) (*TtyConsole, error) {
 
-	tty := &TtyConsole{
-	}
+	tty := &TtyConsole{}
 
 	return tty, nil
 }
 
 func (t *TtyConsole) Resize(h, w int) error {
+	log.Debugln("argon ttyconsole: resize not yet implemented ", h, w)
 	//return term.SetWinsize(t.MasterPty.Fd(), &term.Winsize{Height: uint16(h), Width: uint16(w)})
 	return nil
 }
