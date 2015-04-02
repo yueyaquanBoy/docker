3cefc797b5e50a77a40334e7a948a13636dc86ec
John Howard (VM) <John.Howard@microsoft.com>
2015-04-02 15:05:26 -0700

    Windows: Tidyup of exec driver
    Signed-off-by: John Howard (VM) <John.Howard@microsoft.com>

--- daemon/execdriver/argon/ttyconsole.go
+++ daemon/execdriver/argon/ttyconsole.go
@@ -5,25 +5,19 @@ package argon
 import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/docker/docker/daemon/execdriver"
-	//"github.com/docker/docker/pkg/term"
 )
 
 type TtyConsole struct {
-	//	MasterPty *os.File
-
-	// Think need to store the handle in here somehow. Where the heck from???
 }
 
 func NewTtyConsole(processConfig *execdriver.ProcessConfig, pipes *execdriver.Pipes) (*TtyConsole, error) {
-
 	tty := &TtyConsole{}
-
 	return tty, nil
 }
 
 func (t *TtyConsole) Resize(h, w int) error {
-	log.Debugln("argon ttyconsole: resize not yet implemented ", h, w)
-	//term.SetWinsize
+	log.Debugln("Windows exec ttyconsole: resize not implemented ", h, w)
+	// This needs a call into the HCS to set the virtual TTY.
 	//return term.SetWinsize(t.MasterPty.Fd(), &term.Winsize{Height: uint16(h), Width: uint16(w)})
 	return nil
 }
