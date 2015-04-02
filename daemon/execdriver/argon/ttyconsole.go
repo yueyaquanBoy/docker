b627a8b5869c1528077d6663dfe839d53f535dd0
John Howard (VM) <John.Howard@microsoft.com>
2015-04-02 10:05:09 -0700

    Windows: More terminal logging
    Signed-off-by: John Howard (VM) <John.Howard@microsoft.com>

--- daemon/execdriver/argon/ttyconsole.go
+++ daemon/execdriver/argon/ttyconsole.go
@@ -5,10 +5,13 @@ package argon
 import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/docker/docker/daemon/execdriver"
+	//"github.com/docker/docker/pkg/term"
 )
 
 type TtyConsole struct {
 	//	MasterPty *os.File
+
+	// Think need to store the handle in here somehow. Where the heck from???
 }
 
 func NewTtyConsole(processConfig *execdriver.ProcessConfig, pipes *execdriver.Pipes) (*TtyConsole, error) {
@@ -20,6 +23,7 @@ func NewTtyConsole(processConfig *execdriver.ProcessConfig, pipes *execdriver.Pi
 
 func (t *TtyConsole) Resize(h, w int) error {
 	log.Debugln("argon ttyconsole: resize not yet implemented ", h, w)
+	//term.SetWinsize
 	//return term.SetWinsize(t.MasterPty.Fd(), &term.Winsize{Height: uint16(h), Width: uint16(w)})
 	return nil
 }
