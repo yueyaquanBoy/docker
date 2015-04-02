77218a4534ec168af5a950908a69d1879839df17
John Howard (VM) <John.Howard@microsoft.com>
2015-04-02 16:41:49 -0700

    Windows: Added exec capability to exec driver
    Signed-off-by: John Howard (VM) <John.Howard@microsoft.com>

--- daemon/execdriver/argon/driver.go
+++ daemon/execdriver/argon/driver.go
@@ -11,6 +11,7 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/docker/docker/daemon/execdriver"
+	"github.com/docker/docker/pkg/common"
 	"github.com/docker/docker/pkg/hcsshim"
 	"gopkg.in/natefinch/npipe.v2"
 )
@@ -50,16 +51,6 @@ func NewDriver(root, initPath string) (*driver, error) {
 		initPath:         initPath,
 		activeContainers: make(map[string]*activeContainer),
 	}, nil
-}
-
-func (d *driver) Exec(c *execdriver.Command, processConfig *execdriver.ProcessConfig, pipes *execdriver.Pipes, startCallback execdriver.StartCallback) (int, error) {
-
-	active := d.activeContainers[c.ID]
-	if active == nil {
-		return -1, fmt.Errorf("No active container exists with ID %s", c.ID)
-	}
-
-	return 0, nil
 }
 
 func checkSupportedOptions(c *execdriver.Command) error {
@@ -396,4 +387,135 @@ func (d *driver) Clean(id string) error {
 
 func (d *driver) Stats(id string) (*execdriver.ResourceStats, error) {
 	return nil, fmt.Errorf("Windows: Stats not implemented")
+}
+
+func (d *driver) Exec(c *execdriver.Command, processConfig *execdriver.ProcessConfig, pipes *execdriver.Pipes, startCallback execdriver.StartCallback) (int, error) {
+
+	active := d.activeContainers[c.ID]
+	if active == nil {
+		return -1, fmt.Errorf("No active container exists with ID %s", c.ID)
+	}
+
+	var (
+		term execdriver.Terminal
+		err  error
+	)
+
+	// We use another unique ID here for each exec instance otherwise it
+	// may conflict with the pipe name being used by RUN.
+
+	// We use a different pipe name between real and dummy mode in the HCS
+	var pipePrefix string
+	var randomID string = common.GenerateRandomID()
+
+	if c.Dummy {
+		pipePrefix = `\\.\pipe\` + randomID + `\`
+	} else {
+		pipePrefix = fmt.Sprintf(`\\.\Containers\%s\Device\NamedPipe\%s\`, c.ID, randomID)
+	}
+
+	// This is what gets passed into the exec - structure containing the
+	// names of the named pipes for stdin/out/err
+	stdDevices := hcsshim.Devices{}
+
+	// Connect stdin
+	if pipes.Stdin != nil {
+		stdDevices.StdInPipe = pipePrefix + c.ID + "-stdin"
+
+		// Listen on the named pipe
+		inListen, err = npipe.Listen(stdDevices.StdInPipe)
+		if err != nil {
+			log.Debugln("Failed to listen on ", stdDevices.StdInPipe, err)
+			return -1, err
+		}
+		defer inListen.Close()
+
+		// Launch a goroutine to do the accept. We do this so that we can
+		// cause an otherwise blocking goroutine to gracefully close when
+		// the caller (us) closes the listener
+		go stdinAccept(inListen, stdDevices.StdInPipe, pipes.Stdin)
+	}
+
+	// Connect stdout
+	stdDevices.StdOutPipe = pipePrefix + c.ID + "-stdout"
+	outListen, err = npipe.Listen(stdDevices.StdOutPipe)
+	if err != nil {
+		log.Debugln("Failed to listen on ", stdDevices.StdOutPipe, err)
+		return -1, err
+	}
+	defer outListen.Close()
+	go stdouterrAccept(outListen, stdDevices.StdOutPipe, pipes.Stdout)
+
+	// No stderr on TTY.
+	if !c.ProcessConfig.Tty {
+		// Connect stderr
+		stdDevices.StdErrPipe = pipePrefix + c.ID + "-stderr"
+		errListen, err = npipe.Listen(stdDevices.StdErrPipe)
+		if err != nil {
+			log.Debugln("Failed to listen on ", stdDevices.StdErrPipe, err)
+			return -1, err
+		}
+		defer errListen.Close()
+		go stdouterrAccept(errListen, stdDevices.StdErrPipe, pipes.Stderr)
+	}
+
+	if c.ProcessConfig.Tty {
+		term, err = NewTtyConsole(&c.ProcessConfig, pipes)
+	} else {
+		term, err = NewStdConsole(&c.ProcessConfig, pipes)
+	}
+	processConfig.Terminal = term
+
+	// Sure this would get caught earlier, but just in case - validate that we
+	// have something to run
+	if processConfig.Entrypoint == "" {
+		err = errors.New("No entrypoint specified")
+		log.Debugln(err)
+		return -1, err
+	}
+
+	// Build the command line of the process
+	commandLine := processConfig.Entrypoint
+	for _, arg := range processConfig.Arguments {
+		log.Debugln("appending ", arg)
+		commandLine += " " + arg
+	}
+	log.Debugln("commandLine: ", commandLine)
+
+	// TTY or not is passed into executing a process
+	var emulateTTY uint32 = 0x00000000
+	if c.ProcessConfig.Tty == true {
+		emulateTTY = 0x00000001
+	}
+
+	// Start the command running in the container.
+	var pid uint32
+	pid, err = hcsshim.CreateProcessInComputeSystem(c.ID,
+		commandLine,
+		stdDevices,
+		emulateTTY)
+	if err != nil {
+		log.Debugln("CreateProcessInComputeSystem() failed ", err)
+		return -1, err
+	}
+
+	log.Debugln("PID ", pid)
+
+	// Invoke the start callback
+	if startCallback != nil {
+		startCallback(&c.ProcessConfig, int(pid))
+	}
+
+	var exitCode uint32
+	exitCode, err = hcsshim.WaitForProcessInComputeSystem(c.ID, pid)
+	if err != nil {
+		log.Debugln("Failed to WaitForProcessInComputeSystem ", err)
+		return -1, err
+	}
+
+	// TODO - What do we do with this exit code????
+	log.Debugln("exitcode err", exitCode, err)
+
+	log.Debugln("Exiting Run() with ExitCode 0", c.ID)
+	return int(exitCode), nil
 }
