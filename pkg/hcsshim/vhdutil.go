5cb2616356a7db2be93ba76ed9e5ed1feca83507
John Starks <jostarks@ntdev.microsoft.com>
2015-04-03 11:03:06 -0700

    Improve error messages in hcsshim

--- pkg/hcsshim/vhdutil.go
+++ pkg/hcsshim/vhdutil.go
@@ -6,18 +6,17 @@
 package hcsshim
 
 import (
-	"errors"
 	"syscall"
 	"unsafe"
 
 	log "github.com/Sirupsen/logrus"
 )
 
-const (
-	PROCCREATEDIFFVHD = "CreateDiffVhd"
-	PROCMOUNTVHD      = "MountVhd"
-	PROCDISMOUNTVHD   = "DismountVhd"
-	PROCGETVOLUMEPATH = "GetVhdVolumePath"
+var (
+	procCreateDiffVhd    = modvmcompute.NewProc("CreateDiffVhd")
+	procMountVhd         = modvmcompute.NewProc("MountVhd")
+	procDismountVhd      = modvmcompute.NewProc("DismountVhd")
+	procGetVhdVolumePath = modvmcompute.NewProc("GetVhdVolumePath")
 )
 
 func CreateDiffVhd(newVhdPath, parentVhdPath string) error {
@@ -25,56 +24,25 @@ func CreateDiffVhd(newVhdPath, parentVhdPath string) error {
 	log.Debugln("newVhdPath:", newVhdPath)
 	log.Debugln("parentVhdPath:", parentVhdPath)
 
-	var (
-		// Arguments to the call.
-		newVhdPathp, parentVhdPathp *uint16
-
-		// Result values from calling the procedure.
-		r1, r2 uintptr
-
-		// The DLL and the procedure in the DLL respectively.
-		dll  *syscall.DLL
-		proc *syscall.Proc
-
-		// Error tracking.
-		err error
-	)
-
-	newVhdPathp, err = syscall.UTF16PtrFromString(newVhdPath)
+	newVhdPathp, err := syscall.UTF16PtrFromString(newVhdPath)
 	if err != nil {
 		log.Debugln("Failed conversion of newVhdPath to pointer:", err)
 		return err
 	}
 
-	parentVhdPathp, err = syscall.UTF16PtrFromString(parentVhdPath)
+	parentVhdPathp, err := syscall.UTF16PtrFromString(parentVhdPath)
 	if err != nil {
 		log.Debugln("Failed conversion of parentVhdPath to pointer:", err)
 		return err
 	}
 
-	// Load the DLL and get the CreateComputeSystem function
-	dll, proc, err = loadAndFind(PROCCREATEDIFFVHD)
-
-	// Release once used if we managed to get a handle to it
-	if dll != nil {
-		defer dll.Release()
-	}
-
 	// Call the procedure itself.
-	r1, r2, err = proc.Call(
+	r1, _, _ := procCreateDiffVhd.Call(
 		uintptr(unsafe.Pointer(newVhdPathp)),
 		uintptr(unsafe.Pointer(parentVhdPathp)))
 
-	// Check the result codes first
-	if r1 != 0 || r2 != 0 {
-		// Check for error itself next
-		if err != nil {
-			if err.Error() != "The operation completed successfully." {
-				return errors.New(PROCCREATEDIFFVHD + " failed. " + err.Error())
-			}
-		}
-		log.Debugln("r1 ", r1)
-		return errors.New(PROCCREATEDIFFVHD + " failed r1/r2 check")
+	if r1 != 0 {
+		return syscall.Errno(r1)
 	}
 
 	return nil
@@ -84,112 +52,41 @@ func MountVhd(vhdPath string) (string, error) {
 	log.Debugln("hcsshim::MountVhd")
 	log.Debugln("vhdPath:", vhdPath)
 
-	var (
-		// Resulting string.
-		volumePath string
-
-		// Arguments to the call.
-		vhdPathp    *uint16
-		volumePathp [256]uint16
-
-		// Result values from calling the procedure.
-		r1, r2 uintptr
-
-		// The DLL and the procedure in the DLL respectively.
-		dll  *syscall.DLL
-		proc *syscall.Proc
-
-		// Error tracking.
-		err error
-	)
-
-	volumePathp[0] = 0
-
-	vhdPathp, err = syscall.UTF16PtrFromString(vhdPath)
+	vhdPathp, err := syscall.UTF16PtrFromString(vhdPath)
 	if err != nil {
 		log.Debugln("Failed conversion of vhdPath to pointer:", err)
 		return "", err
 	}
 
-	// Load the DLL and get the CreateComputeSystem function
-	dll, proc, err = loadAndFind(PROCMOUNTVHD)
-
-	// Release once used if we managed to get a handle to it
-	if dll != nil {
-		defer dll.Release()
-	}
-
+	var volumePathp [256]uint16
+	volumePathp[0] = 0
 	// Call the procedure itself.
-	r1, r2, err = proc.Call(
+	r1, _, _ := procMountVhd.Call(
 		uintptr(unsafe.Pointer(vhdPathp)),
 		uintptr(unsafe.Pointer(&volumePathp)))
 
-	// Check the result codes first
-	if r1 != 0 || r2 != 0 {
-		// Check for error itself next
-		if err != nil {
-			if err.Error() != "The operation completed successfully." {
-				return "", errors.New(PROCMOUNTVHD + " failed. " + err.Error())
-			}
-		}
-		log.Debugln("r1 ", r1)
-		return "", errors.New(PROCMOUNTVHD + " failed r1/r2 check")
+	if r1 != 0 {
+		return "", syscall.Errno(r1)
 	}
 
-	if volumePathp[0] != 0 {
-		volumePath = syscall.UTF16ToString(volumePathp[0:])
-	}
-
-	return volumePath, nil
+	return syscall.UTF16ToString(volumePathp[0:]), nil
 }
 
 func DismountVhd(vhdPath string) error {
 	log.Debugln("hcsshim::DismountVhd")
 	log.Debugln("vhdPath:", vhdPath)
 
-	var (
-		// Arguments to the call.
-		vhdPathp *uint16
-
-		// Result values from calling the procedure.
-		r1, r2 uintptr
-
-		// The DLL and the procedure in the DLL respectively.
-		dll  *syscall.DLL
-		proc *syscall.Proc
-
-		// Error tracking.
-		err error
-	)
-
-	vhdPathp, err = syscall.UTF16PtrFromString(vhdPath)
+	vhdPathp, err := syscall.UTF16PtrFromString(vhdPath)
 	if err != nil {
 		log.Debugln("Failed conversion of vhdPath to pointer:", err)
 		return err
 	}
 
-	// Load the DLL and get the CreateComputeSystem function
-	dll, proc, err = loadAndFind(PROCDISMOUNTVHD)
-
-	// Release once used if we managed to get a handle to it
-	if dll != nil {
-		defer dll.Release()
-	}
-
-	// Call the procedure itself.
-	r1, r2, err = proc.Call(
+	r1, _, _ := procDismountVhd.Call(
 		uintptr(unsafe.Pointer(vhdPathp)))
 
-	// Check the result codes first
-	if r1 != 0 || r2 != 0 {
-		// Check for error itself next
-		if err != nil {
-			if err.Error() != "The operation completed successfully." {
-				return errors.New(PROCDISMOUNTVHD + " failed. " + err.Error())
-			}
-		}
-		log.Debugln("r1 ", r1)
-		return errors.New(PROCDISMOUNTVHD + " failed r1/r2 check")
+	if r1 != 0 {
+		return syscall.Errno(r1)
 	}
 
 	return nil
@@ -199,61 +96,21 @@ func GetVhdVolumePath(vhdPath string) (string, error) {
 	log.Debugln("hcsshim::GetVhdVolumePath")
 	log.Debugln("vhdPath:", vhdPath)
 
-	var (
-		// Resulting string.
-		volumePath string
-
-		// Arguments to the call.
-		vhdPathp    *uint16
-		volumePathp [256]uint16
-
-		// Result values from calling the procedure.
-		r1, r2 uintptr
-
-		// The DLL and the procedure in the DLL respectively.
-		dll  *syscall.DLL
-		proc *syscall.Proc
-
-		// Error tracking.
-		err error
-	)
-
-	volumePathp[0] = 0
-
-	vhdPathp, err = syscall.UTF16PtrFromString(vhdPath)
+	vhdPathp, err := syscall.UTF16PtrFromString(vhdPath)
 	if err != nil {
 		log.Debugln("Failed conversion of vhdPath to pointer:", err)
 		return "", err
 	}
 
-	// Load the DLL and get the CreateComputeSystem function
-	dll, proc, err = loadAndFind(PROCGETVOLUMEPATH)
-
-	// Release once used if we managed to get a handle to it
-	if dll != nil {
-		defer dll.Release()
-	}
-
-	// Call the procedure itself.
-	r1, r2, err = proc.Call(
+	var volumePathp [256]uint16
+	volumePathp[0] = 0
+	r1, _, _ := procGetVhdVolumePath.Call(
 		uintptr(unsafe.Pointer(vhdPathp)),
 		uintptr(unsafe.Pointer(&volumePathp)))
 
-	// Check the result codes first
-	if r1 != 0 || r2 != 0 {
-		// Check for error itself next
-		if err != nil {
-			if err.Error() != "The operation completed successfully." {
-				return "", errors.New(PROCGETVOLUMEPATH + " failed. " + err.Error())
-			}
-		}
-		log.Debugln("r1 ", r1)
-		return "", errors.New(PROCGETVOLUMEPATH + " failed r1/r2 check")
+	if r1 != 0 {
+		return "", syscall.Errno(r1)
 	}
 
-	if volumePathp[0] != 0 {
-		volumePath = syscall.UTF16ToString(volumePathp[0:])
-	}
-
-	return volumePath, nil
+	return syscall.UTF16ToString(volumePathp[0:]), nil
 }
