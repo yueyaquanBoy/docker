cac69c80ab6a81b356de66e979b3b52f08fae3d3
John Howard <jhoward@microsoft.com>
2015-05-05 12:09:04 -0700

    Linux compile error fixes exec_linux.go
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- daemon/exec_linux.go
+++ daemon/exec_linux.go
@@ -3,6 +3,8 @@
 package daemon
 
 import (
+	"strings"
+
 	"github.com/docker/docker/daemon/execdriver/lxc"
 )
 
@@ -10,4 +12,5 @@ func lxcCheck(DriverName string) error {
 	if strings.HasPrefix(DriverName, lxc.DriverName) {
 		return lxc.ErrExec
 	}
+	return nil
 }
