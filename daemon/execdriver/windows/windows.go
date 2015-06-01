053ab37331d4ea399632c868b83a5c31cf391e65
John Howard <jhoward@microsoft.com>
2015-06-01 13:14:47 -0700

    Revert "Windows: Exec driver (alpha) plus support"
    This reverts commit d3bc5765acf7f1a9abdeb57d9881da495224cb4e.

--- daemon/execdriver/windows/windows.go
+++ daemon/execdriver/windows/windows.go
@@ -1,26 +1,23 @@
 // +build windows
 
-package windows
+/*
+ This is the Windows driver for containers.
 
-// Note this is alpha code for the bring up of containers on Windows.
+ TODO Windows: It is currently a placeholder to allow compilation of the
+ daemon. Future PRs will have an implementation of this driver.
+*/
+
+package windows
 
 import (
 	"fmt"
-	"strings"
-	"sync"
 
-	"github.com/Sirupsen/logrus"
 	"github.com/docker/docker/daemon/execdriver"
-	"github.com/docker/docker/pkg/parsers"
 )
 
-// This is a daemon development variable only and should not be
-// used for running production containers on Windows.
-var dummyMode bool
-
 const (
-	DriverName = "Windows 1854"
-	Version    = "Alpha 10130"
+	DriverName = "Windows"
+	Version    = "Placeholder"
 )
 
 type activeContainer struct {
@@ -28,40 +25,73 @@ type activeContainer struct {
 }
 
 type driver struct {
-	root             string
-	initPath         string
-	activeContainers map[string]*activeContainer
-	sync.Mutex
+	root     string
+	initPath string
+}
+
+type info struct {
+	ID     string
+	driver *driver
+}
+
+func NewDriver(root, initPath string) (*driver, error) {
+	return &driver{
+		root:     root,
+		initPath: initPath,
+	}, nil
+}
+
+func (d *driver) Run(c *execdriver.Command, pipes *execdriver.Pipes, startCallback execdriver.StartCallback) (execdriver.ExitStatus, error) {
+	return execdriver.ExitStatus{ExitCode: 0}, nil
+}
+
+func (d *driver) Terminate(p *execdriver.Command) error {
+	return nil
+}
+
+func (d *driver) Kill(p *execdriver.Command, sig int) error {
+	return nil
+}
+
+func kill(ID string, PID int) error {
+	return nil
+}
+
+func (d *driver) Pause(c *execdriver.Command) error {
+	return fmt.Errorf("Windows: Containers cannot be paused")
+}
+
+func (d *driver) Unpause(c *execdriver.Command) error {
+	return fmt.Errorf("Windows: Containers cannot be paused")
+}
+
+func (i *info) IsRunning() bool {
+	return false
+}
+
+func (d *driver) Info(id string) execdriver.Info {
+	return &info{
+		ID:     id,
+		driver: d,
+	}
 }
 
 func (d *driver) Name() string {
-	return fmt.Sprintf("%s %s", DriverName, Version)
+	return fmt.Sprintf("%s Date %s", DriverName, Version)
 }
 
-func NewDriver(root, initPath string, options []string) (*driver, error) {
+func (d *driver) GetPidsForContainer(id string) ([]int, error) {
+	return nil, fmt.Errorf("GetPidsForContainer: GetPidsForContainer() not implemented")
+}
 
-	for _, option := range options {
-		key, val, err := parsers.ParseKeyValueOpt(option)
-		if err != nil {
-			return nil, err
-		}
-		key = strings.ToLower(key)
-		switch key {
+func (d *driver) Clean(id string) error {
+	return nil
+}
 
-		case "dummy":
-			switch val {
-			case "1":
-				dummyMode = true
-				logrus.Warn("Using dummy mode in Windows exec driver Exec(). This is for development use only!")
-			}
-		default:
-			return nil, fmt.Errorf("Unknown exec driver option %s\n", key)
-		}
-	}
+func (d *driver) Stats(id string) (*execdriver.ResourceStats, error) {
+	return nil, fmt.Errorf("Windows: Stats not implemented")
+}
 
-	return &driver{
-		root:             root,
-		initPath:         initPath,
-		activeContainers: make(map[string]*activeContainer),
-	}, nil
+func (d *driver) Exec(c *execdriver.Command, processConfig *execdriver.ProcessConfig, pipes *execdriver.Pipes, startCallback execdriver.StartCallback) (int, error) {
+	return 0, nil
 }
